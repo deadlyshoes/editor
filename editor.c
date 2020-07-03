dcb8db468c718fb7d45bc9c30bbfaf7914bc38bd
deadlyshoes <khoseilmar@gmail.com>
2020-07-03 18:32:02 -0300

    Keep horizontal cursor position on row change

--- editor.c
+++ editor.c
@@ -1007,6 +1007,7 @@ char *editorPrompt(char *prompt, void (*callback)(char *, int)) {
 }
 
 void editorMoveCursor(int key) {
+	static int keep_rx = 0; // Maintain cursor position on row change
 	erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
 
 	switch (key) {
@@ -1017,6 +1018,7 @@ void editorMoveCursor(int key) {
 				E.cy--;
 				E.cx = E.row[E.cy].size;
 			}
+			keep_rx = editorRowCxToRx(row, E.cx);
 			break;
 		case ARROW_RIGHT: 
 			if (row && E.cx < row->size) {
@@ -1025,19 +1027,17 @@ void editorMoveCursor(int key) {
 				E.cy++;
 				E.cx = 0;
 			}
+			keep_rx = editorRowCxToRx(row, E.cx);
 			break;
 		case ARROW_UP:
 			if (E.cy != 0) {
-				int cur_rx = editorRowCxToRx(&E.row[E.cy], E.cx);
-				E.cx = editorRowRxToCx(&E.row[E.cy - 1], cur_rx);
-
+				E.cx = editorRowRxToCx(&E.row[E.cy - 1], keep_rx);
 				E.cy--;
 			}
 			break;
 		case ARROW_DOWN:
 			if (E.cy < E.numrows) {
-				int cur_rx = editorRowCxToRx(&E.row[E.cy], E.cx);
-				E.cx = editorRowRxToCx(&E.row[E.cy + 1], cur_rx);
+				E.cx = editorRowRxToCx(&E.row[E.cy + 1], keep_rx);
 				E.cy++;
 			}
 			break;
